7c28baeca111ea2566acdbc4cc9fb4b55c46d21e
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-11 11:29:29 +0000

    - Build initial Knuth sequence: gap = gap * 3 + 1 - Main loop: gap 0 or 1 - - Required print - - Reduce gap using Knuth reverse formula

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,4 +1,3 @@
-
 #include "sort.h"
 
 /**
@@ -9,31 +8,30 @@
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, i, j;
-	int tmp;
+	int temp;
 
 	if (!array || size < 2)
 		return;
-
 	while (gap < size / 3)
-		gap = (gap - 1) / 3;
+		gap = gap * 3 + 1;
 
 	while (gap >= 1)
 	{
 		for (i = gap; i < size; i++)
 		{
-			tmp = array[i];
+			temp = array[i];
 			j = i;
 
-			while (j >= gap && array[j - gap] > tmp)
-			{
-				array[j] = array[j - gap];
-				j -= gap;
-			}
-			array[j] = tmp;
+		while (j >= gap && array[j - gap] > temp)
+		{
+			array[j] = array[j - gap];
+			j -= gap;
+		}
+
+		array[j] = temp;
 		}
 
 		print_array(array, size);
-
 		gap = (gap - 1) / 3;
 	}
 }
