03cb885acdb20d22437cdf2eb39c60b9308694b8
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-11 10:07:32 +0000

    - Find the minimum element in unsorted portion - Swap if minimum is not at current position - - Print array after swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,38 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array using selection sort algorithm
- * @array: Array of integers
- * @size: Size of the array
+ * selection_sort - sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm
+ * @array: pointer to the array to sort
+ * @size: size of the array
+ *
+ * Description: Prints the array after each swap
  */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_index;
-int tmp;
+	size_t i, j, min_idx;
+	int temp;
 
-if (!array || size < 2)
-	return;
+	if (array == NULL || size < 2)
+		return;
 
-for (i = 0; i < size - 1; i++)
-{
-	min_index = i;
-
-	for (j = i + 1; j < size; j++)
+	for (i = 0; i < size - 1; i++)
 	{
-		if (array[j] < array[min_index])
-			min_index = j;
-	}
+		min_idx = i;
 
-	if (min_index != i)
-	{
-		tmp = array[i];
-		array[i] = array[min_index];
-		array[min_index] = tmp;
-		print_array(array, size);
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+
+			print_array(array, size);
+		}
 	}
-}
 }
