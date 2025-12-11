5475d74a5fc2f4baa9b6673c0a206065c756b9ac
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-12-11 10:03:11 +0000

    #include <unistd.h> #include <fcntl.h> #include <sys/types.h> #include <sys/stat.h>

--- sort.h
+++ sort.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * struct listint_s - Doubly linked list node
